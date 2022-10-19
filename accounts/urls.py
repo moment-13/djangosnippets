cb88911ec84a436e64c199812909de12e9a5c3b9
moment-13 <n.wisteria.14@gmail.com>
2022-10-20 06:37:01 +0900

    1.6.2 finished

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 
-urlpatterns = {
+urlpatterns = [
     path('login/', LoginView.as_view(redirect_authenticated_user=True, template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-}
+]
 
 
 
